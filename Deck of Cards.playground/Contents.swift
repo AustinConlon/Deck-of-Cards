e0af9dc83e8140b40512d0e0893dfc3b7390e6d5
Austin Conlon <austinconlon@icloud.com>
2020-01-12 21:20:39 -0800

    Give rank and suit enumerations raw values, nest them within the card structure

--- Deck of Cards.playground/Contents.swift
+++ Deck of Cards.playground/Contents.swift
@@ -1,12 +1,13 @@
-enum Rank: CaseIterable {
-    case ace, two, three, four, five, six, seven, eight, nine, ten, jack, queen, king
-}
-
-enum Suit: CaseIterable {
-    case clubs, diamonds, hearts, spades
-}
-
 struct Card {
+    enum Rank: Int, CaseIterable {
+        case two = 2, three, four, five, six, seven, eight, nine, ten
+        case ace, jack, queen, king
+    }
+
+    enum Suit: Character, CaseIterable {
+        case clubs = "♣️", diamonds = "♦️", hearts = "♥️", spades = "♠️"
+    }
+    
     let rank: Rank
     let suit: Suit
 }
@@ -15,8 +16,8 @@ struct Deck {
     var cards = [Card]()
     
     init() {
-        for rank in Rank.allCases {
-            for suit in Suit.allCases {
+        for rank in Card.Rank.allCases {
+            for suit in Card.Suit.allCases {
                 cards.append(Card(rank: rank, suit: suit))
             }
         }
